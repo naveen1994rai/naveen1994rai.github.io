42b3c82b0997cfc0d005031b8833c6a818e725c3
Naveen Rai <naveen.rai@dtone.com>
2021-05-19 10:10:00 +0530

    initial commit for this repo.

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let autohide = document.querySelector('.autohide');
+    
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+  
+    if(autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                autohide.classList.remove('scrolled-down');
+                autohide.classList.add('scrolled-up');
+            }
+            else {
+                autohide.classList.remove('scrolled-up');
+                autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+    }
+  }); 
